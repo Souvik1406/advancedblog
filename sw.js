c9210b0fe40f2cf716bf4a0660c56b1193daa770
Souvik Roy <70591967+Souvik1406@users.noreply.github.com>
2021-08-03 19:06:56 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+self.addEventListener('install',function(event){
+    event.waitUntil(
+        caches.open('sw-cache').then(function(cache){
+            return cache.add('index.html',
+            'css/style.css',
+            'js/script.js',
+            'thankyou.html',
+            'images/about-img.jpg',
+            'images/home-imh.jpg',
+            'images/img-1.jpg',
+            'images/img-2.jpg',
+            'images/img-3.jpg',
+            'images/img-4.jpg',
+            'images/img-5.jpg',
+            'images/img-6.jpg',
+            'images/wdicon1.png',
+            'images/wdicon2.png',
+            'sw.js',
+            'manifest.json'
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch',function(event){
+    event.respondwith(
+        caches.match(event.request).then(function(response){
+            return response || fetch(event.request);
+        })
+    );
+});
